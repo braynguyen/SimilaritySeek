38124048317e718e48d9cc23811bfec7bf984a4c
Jophy Chon Hou Ye <jophyyjh@gmail.com>
2024-09-14 17:16:45 -0400

    Rename my-app/ -> frontend/, exampleSimpleFlask/ -> backend/

--- backend/flaskBackend.py
+++ backend/flaskBackend.py
@@ -95,8 +95,6 @@ def insert():
     return jsonify({"response": "new information added"})
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5010)
-
+    app.run(debug=True, host='0.0.0.0', port=5010)
 
